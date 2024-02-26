e4b2bc839265c11851247975df1175c7f8140042
Pedro Freitas <phfreitas@ges.inatel.br>
2024-02-26 18:40:43 -0300

    add file `main.py` with a test case

---
+++ main.py
@@ -0,0 +1,11 @@
+from Professor import Professor
+from Aula import Aula
+from Aluno import Aluno
+
+professor = Professor("Lucas")
+aluno1 = Aluno("Maria")
+aluno2 = Aluno("Pedro")
+aula = Aula(professor, "Programação Orientada a Objetos")
+aula.adicionar_aluno(aluno1)
+aula.adicionar_aluno(aluno2)
+print(aula.listar_presenca())
